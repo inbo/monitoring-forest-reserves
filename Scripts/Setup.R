b1bd1984833ddb8d6c857de70af8a8918d998cad
Anja Leyman <anja.leyman@inbo.be>
2024-09-26 10:37:19 +0200

    setup: extra info over packages

--- Scripts/Setup.R
+++ Scripts/Setup.R
@@ -1,5 +1,35 @@
-# install.packages("forrescalc") # samen met INBO universe: update packages dan wordt deze ook geupdate
-# enkel als nieuwe versie uitkomt ...
+## INSTALL PACKAGES ----------------
+
+# install.packages("forrescalc") 
+        # samen met INBO universe: update packages dan wordt deze ook geupdate
+        # enkel als nieuwe versie uitkomt ...
+
+# remotes::install_github("inbo/forrescalc@bugfix")   # ok  26/9/2024 (in aparte R)
+
+    # tijdelijk om probleem met save to access op te lossen 
+    # mail ELs 25/9/2024:
+        # Ik heb dit probleempje opgelost met een commit in een nieuwe branch 'bugfix', 
+        # dus je kan deze versie installeren met
+        # 
+        # remotes::install_github("inbo/forrescalc@bugfix")
+        # 
+        # Nadeel is wel dat dit geen officiële versie met versienummer is, dus voor het wegschrijven van 
+        # gegevens in forresdat gebruik je beter de versie die je installeert met install.packages() omdat je er 
+        # dan een proper versienummer bij hebt  (je kan gewoon telkens opnieuw met die commando's installeren wat 
+        # je nodig hebt voor je code, misschien minder handig maar ik wil niet voor elk detail een nieuwe versie maken)
+        # 
+        # Ik zou zeggen: test alles goed uit (zeker met Access) en lees alles goed na, dan laat ik deze branch
+        # eventjes open voor eventuele extra problemen, en als alles over enkele dagen of weken in orde lijkt,
+        # zal ik deze als nieuwe versie publiceren.  (En ik zal meteen ook dat foutje in de documentatie
+        # verbeteren, maar ga wachten met pushen tot vanavond of de volgende commit, kwestie om de
+        # continuous integration op github niet te overladen.)
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!! Bij export to forersdat terug andere installeren, met versienr !!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+## LIBRARIES ----------------
 
 library(forrescalc)
 # https://github.com/inbo/forresdat
