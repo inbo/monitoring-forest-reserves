198cdcafce814c4e0c4fbf2ee4b9c8881ae2a146
Anja Leyman <anja.leyman@inbo.be>
2026-03-19 17:43:30 +0100

    aanmaak virtuele bomen terrijst

--- Scripts/Per_gebied/Terrijst/setup_paths_gebied.R
+++ Scripts/Per_gebied/Terrijst/setup_paths_gebied.R
@@ -8,18 +8,21 @@ forestreserve_short <- "Terrijst"
 # forestreserve_short <- "Wijnendale"
 # plot_type <- "CA"
 # plot_type <- "CP"
-forestreserve_folder <- paste0(path_to_reserves, "06_Terrijst")
+forestreserve_folder_pc <- paste0(path_to_reserves, "06_Terrijst")  # daar staan de fieldmap-projecten
+forestreserve_folder_teamdrive <- paste0(path_to_teamdrive, "PRJ_BR_Gegevensverwerking/20_Per_gebied/2026_", forestreserve_short)
 # jaar_analyse <- "2021"
 
 
-###########
-path_to_datarequest <- paste0(forestreserve_folder, "/verwerking_2026_1e2e3eSET/output_forrescalc/") # c-schijf
-path_to_teamdrive_gebied <- paste0(path_to_teamdrive, "PRJ_BR_Gegevensverwerking/20_Per_gebied/2026_", forestreserve_short, "/")
-
 # plots_to_exclude <- c(701, 707, 709, 727, 728, 738, 739, 740, 759, 760)
 
-path_to_datacontrol <- paste0(path_to_teamdrive, "PRJ_BR_Gegevensverwerking/per_gebied/2026_", forestreserve_short, "/01_datacontrole/")
-path_to_heightcurves <- paste0(path_to_teamdrive, "PRJ_BR_Gegevensverwerking/per_gebied/2026_", forestreserve_short, "/02_dh_curves/")
+path_to_datacontrol <- paste0(forestreserve_folder_teamdrive, "/01_datacontrole/")
+path_to_heightcurves <- paste0(forestreserve_folder_teamdrive, "/02_dh_curves/")
+
+path_to_plotdata_teamdrive <- paste0(forestreserve_folder_teamdrive, "/03_output_R_plotlevel")
+
+# EVENTUEEL NIET MEER GEBRUIKEN??
+path_to_plotdata_pc <- paste0(forestreserve_folder_pc, "/verwerking_2026_1e2e3eSET/output_forrescalc/") # c-schijf
+
 
 # TIJDELIJK `path_to_fieldmap_db` naar nieuwe situatie mrt 2026 (met aanpassing nalv andere benadering hakhout)
 path_to_fieldmap <- paste0(path_to_databases, "MDB_BR_22_inbo2026_20260313/")
