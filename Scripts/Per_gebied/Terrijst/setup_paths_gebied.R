c36a9714c4b79b7d49493b11f64877d9a263f8ab
Anja Leyman <anja.leyman@inbo.be>
2026-03-24 14:38:03 +0100

    terrijst dendro + start reg en veg

--- Scripts/Per_gebied/Terrijst/setup_paths_gebied.R
+++ Scripts/Per_gebied/Terrijst/setup_paths_gebied.R
@@ -18,7 +18,7 @@ forestreserve_folder_teamdrive <- paste0(path_to_teamdrive, "PRJ_BR_Gegevensverw
 path_to_datacontrol <- paste0(forestreserve_folder_teamdrive, "/01_datacontrole/")
 path_to_heightcurves <- paste0(forestreserve_folder_teamdrive, "/02_dh_curves/")
 
-path_to_plotdata_teamdrive <- paste0(forestreserve_folder_teamdrive, "/03_output_R_plotlevel")
+path_to_plotdata_teamdrive <- paste0(forestreserve_folder_teamdrive, "/03_output_R_plotlevel/")
 
 # EVENTUEEL NIET MEER GEBRUIKEN??
 path_to_plotdata_pc <- paste0(forestreserve_folder_pc, "/verwerking_2026_1e2e3eSET/output_forrescalc/") # c-schijf
